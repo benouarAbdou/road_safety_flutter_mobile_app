24cd2beb59bd3c4f29495384cc7509be2b2dc482
benouarAbdou <benouarabdelouahab@gmail.com>
2024-11-18 19:35:43 +0100

    first version

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Car Speed Tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Car Speed Tracker'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late StreamSubscription<Position> _positionStream;
+  double _speed = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _startTracking();
+  }
+
+  void _startTracking() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return; // Location services are not enabled
+    }
+
+    // Check location permissions
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return; // Permissions are denied
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return; // Permissions are permanently denied
+    }
+
+    // Start listening to position updates
+    _positionStream = Geolocator.getPositionStream(
+      locationSettings: const LocationSettings(
+        distanceFilter: 8,
+        accuracy: LocationAccuracy.best,
+      ),
+    ).listen((Position? position) {
+      if (position != null) {
+        _onSpeedChange((position.speed * 18) / 5); // Convert m/s to km/h
+      }
+    });
+  }
+
+  void _onSpeedChange(double newSpeed) {
+    setState(() {
+      _speed = newSpeed < 3.5 ? 0.0 : newSpeed;
+    });
+  }
+
+  @override
+  void dispose() {
+    _positionStream.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Current Speed:',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '${_speed.toStringAsFixed(0)} km/h',
+              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
