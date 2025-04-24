71e867f1351778d807a597f494c512d3bdf18cc8
benouarAbdou <benouarabdelouahab@gmail.com>
2025-04-24 14:23:39 +0200

    fix login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:my_pfe/controllers/AuthController.dart';
 import 'package:my_pfe/pages/LoginPage.dart';
 import 'package:my_pfe/navigationMenu.dart'; // Import NavigationMenu
 import 'package:my_pfe/theme/theme.dart';
@@ -9,6 +11,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // Register FirebaseAuthController with GetX
+  Get.put(FirebaseAuthController());
   runApp(const MyApp());
 }
 
